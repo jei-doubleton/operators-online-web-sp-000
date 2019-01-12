ecb1149b2b4ab293990a7e4944064f6cdc7b04d1
J <jennifer.singleton@gmail.com>
2019-01-12 18:29:58 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,14 @@
 def unsafe?(speed)
-	speed > 60 || speed < 40 ? true : false
+	if speed > 60 
+		true
+	elsif speed < 40 
+		true
+	else
+		false
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40 ? true : false
 end
